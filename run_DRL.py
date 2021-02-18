06218162eae91e0e07f8f2dba6f55903ce0ff648
KiriteeGak <kiriteegak@gmail.com>
2021-02-18 11:25:25 +0530

    backup

--- run_DRL.py
+++ run_DRL.py
@@ -12,11 +12,12 @@ from config.config import *
 from model.models import *
 import os
 
+
 def run_model() -> None:
     """Train the model."""
 
     # read and preprocess data
-    preprocessed_path = "done_data.csv"
+    preprocessed_path = "done_data_2.csv"
     if os.path.exists(preprocessed_path):
         data = pd.read_csv(preprocessed_path, index_col=0)
     else:
@@ -26,25 +27,31 @@ def run_model() -> None:
 
     print(data.head())
     print(data.size)
+    data = data[['datadate', 'tic', 'close', 'volume', 'macd',
+                 'rsi', 'cci', 'adx', 'turbulence']]
 
     # 2015/10/01 is the date that validation starts
     # 2016/01/01 is the date that real trading starts
     # unique_trade_date needs to start from 2015/10/01 for validation purpose
-    unique_trade_date = data[(data.datadate > 20151001)&(data.datadate <= 20200707)].datadate.unique()
+    # unique_trade_date = data[(data.datadate > 20151001)&(data.datadate <= 20200707)].datadate.unique()
+    data = data.drop_duplicates(subset=['datadate'])
+    unique_trade_date = data[(data.datadate >= 20200601000000) & (data.datadate < 20210101000000)].datadate.unique()
+
     print(unique_trade_date)
 
     # rebalance_window is the number of months to retrain the model
     # validation_window is the number of months to validation the model and select for trading
-    rebalance_window = 63
-    validation_window = 63
-    
+    rebalance_window = int(63 * 24 * 60 / 5)
+    validation_window = int(63 * 24 * 60 / 5)
+
     ## Ensemble Strategy
-    run_ensemble_strategy(df=data, 
-                          unique_trade_date= unique_trade_date,
-                          rebalance_window = rebalance_window,
+    run_ensemble_strategy(df=data,
+                          unique_trade_date=unique_trade_date,
+                          rebalance_window=rebalance_window,
                           validation_window=validation_window)
 
-    #_logger.info(f"saving model version: {_version}")
+    # _logger.info(f"saving model version: {_version}")
+
 
 if __name__ == "__main__":
     run_model()
